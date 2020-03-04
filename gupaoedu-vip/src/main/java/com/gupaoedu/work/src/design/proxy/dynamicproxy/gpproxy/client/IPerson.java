be4e0da7a4951d991dad6d393bf112cb5c3a5674
1015569537@qq.com <1015569537@qq.com>
2020-03-04 19:11:54 +0800

    design

---
+++ gupaoedu-vip/src/main/java/com/gupaoedu/work/src/design/proxy/dynamicproxy/gpproxy/client/IPerson.java
@@ -0,0 +1,12 @@
+package design.proxy.dynamicproxy.gpproxy.client;
+
+
+public interface IPerson {
+
+    void findLove();
+    void findLoveByContidions(String contidions);
+
+
+
+
+}
---
+++ gupaoedu-vip/src/main/java/com/gupaoedu/work/src/design/proxy/dynamicproxy/gpproxy/client/Zhangsan.java
@@ -0,0 +1,16 @@
+package design.proxy.dynamicproxy.gpproxy.client;
+
+
+public class Zhangsan implements IPerson {
+
+    public void findLove() {
+        System.out.println("张三要求：肤白貌美大长腿");
+    }
+
+    @Override
+    public void findLoveByContidions(String contidions) {
+        System.out.println("带参数带方法");
+    }
+
+
+}
